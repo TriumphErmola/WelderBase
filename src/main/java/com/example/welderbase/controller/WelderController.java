7c2e120fc2141a1bf7d3814401c48f1da94e2555
TriumphErmola <xramovic@yandex.ru>
2022-08-15 22:19:29 +0600

    upd

--- src/main/java/com/example/welderbase/controller/WelderController.java
+++ src/main/java/com/example/welderbase/controller/WelderController.java
@@ -6,7 +6,6 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping("/welder")
 public class WelderController {
 
     private final WelderServiceImpl welderService;
