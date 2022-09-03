e19ccd048b8d7bb5034781fafe0857609c29b824
TriumphErmola <xramovic@yandex.ru>
2022-09-03 20:07:06 +0600

    upd

--- src/main/java/com/example/welderbase/service/WelderServiceImpl.java
+++
@@ -1,6 +0,0 @@
-package com.example.welderbase.service;
-
-import com.example.welderbase.model.Welder;
-
-public interface WelderServiceImpl extends CrudService <Welder,Long>{
-}
--- src/main/java/com/example/welderbase/service/map/AbstractMapService.java
+++
@@ -1,8 +0,0 @@
-package com.example.welderbase.service.map;
-
-import com.example.welderbase.model.BaseEntity;
-
-public abstract class AbstractMapService<T extends BaseEntity, ID extends Long> {
-
-
-}
