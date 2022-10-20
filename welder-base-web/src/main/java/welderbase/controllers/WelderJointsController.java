fb3df0411366110ec6157cd3d7763c0a9d325ca9
TriumphErmola <xramovic@yandex.ru>
2022-10-20 21:09:06 +0600

    Обновил контроллер по стыкам.Все работает.

--- welder-base-web/src/main/java/welderbase/controllers/WelderJointsController.java
+++ welder-base-web/src/main/java/welderbase/controllers/WelderJointsController.java
@@ -1,13 +1,22 @@
 package welderbase.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import welderbase.repository.WelderJointsRepository;
 
 @Controller
 public class WelderJointsController {
 
+    private final WelderJointsRepository welderJointsRepository;
+
+    public WelderJointsController(WelderJointsRepository welderJointsRepository) {
+        this.welderJointsRepository = welderJointsRepository;
+    }
+
     @RequestMapping({"/indexwelderjoints","indexWelderJoints.hmtl"})
-    public String indexWelderJoints(){
-        return "indexWelderJoints";
+    public String indexWelderJoints(Model model){
+        model.addAttribute("welderJoints",welderJointsRepository.findAll());
+        return "welderJoints/list";
     }
 }
