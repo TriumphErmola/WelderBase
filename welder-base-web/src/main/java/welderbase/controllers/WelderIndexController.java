fe9ba5e106cc407ce65520578ce51440ff91c331
TriumphErmola <xramovic@yandex.ru>
2022-11-09 21:35:55 +0600

    12 Глава

--- welder-base-web/src/main/java/welderbase/controllers/WelderIndexController.java
+++ welder-base-web/src/main/java/welderbase/controllers/WelderIndexController.java
@@ -3,7 +3,11 @@ package welderbase.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.WebDataBinder;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 import welderbase.repositories.WelderRepository;
 
 @Controller
@@ -15,10 +19,27 @@ public class WelderIndexController {
         this.welderRepository = welderRepository;
     }
 
+    public void setAllowedFields(WebDataBinder dataBinder) {
+        dataBinder.setDisallowedFields("id");
+    }
+
 
     @RequestMapping({"/indexwelder", "indexwelder.hmtl"})
     public String indexWelder(Model model) {
         model.addAttribute("welders", welderRepository.findAll());
-            return "welders/index";
+        return "welders/index";
+    }
+
+    @RequestMapping("/find")
+    public String findWelders() {
+        return "NotImplemented";
+    }
+
+    @GetMapping("/{welderId}")
+    public ModelAndView showWelder(@PathVariable("welderId") Long welderId) {
+        ModelAndView modelAndView = new ModelAndView("welders/weldersDetails");
+        modelAndView.addObject(welderRepository.findById(welderId));
+        return modelAndView;
     }
 }
+
